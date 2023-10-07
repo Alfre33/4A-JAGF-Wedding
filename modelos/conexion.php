ac75c76488c16d5520eb6c732a480512c8786b5d
Alfre33 <josegaspr28383@gmail.com>
2023-10-06 22:40:35 -0600

    primer subida

---
+++ modelos/conexion.php
@@ -0,0 +1,12 @@
+<?php 
+    class Conexion{
+        static public function conectar(){
+            $link = new PDO("mysql:host=localhost:3008;dbname=wedding", "soporte", "soporte");
+    
+            $link->exec("set names utf8");
+    
+            return $link;
+        }
+    }
+    
+?>
